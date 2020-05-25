d8547d5a6fa807e6c7c16a69a65e4591c0f0edc5
fusso <koki19931108@yahoo.co.jp>
2020-05-26 00:46:56 +0900

    dev

---
+++ Assets/_Complete-Game/Scripts/Player/VRPlayerShooting.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+namespace CompleteProject
+{
+    public class VRPlayerShooting : MonoBehaviour
+    {
+
+        public SteamVR_Input_Sources HandType;
+        public SteamVR_Action_Boolean ShootAction;
+
+        public int damagePerShot = 20;
+        public float timeBetweenBullets = 0.15f;
+        public float range = 100f;
+
+        float timer;
+        Ray shootRay = new Ray();
+        RaycastHit shootHit;
+        int shootableMask;
+        ParticleSystem gunParticles;
+        LineRenderer gunLine;
+        AudioSource gunAudio;
+        Light gunLight;
+        float effectDisplayTime = 0.2f;
+
+        // Use this for initialization
+        void Awake()
+        {
+            shootableMask = LayerMask.GetMask("Shootable");
+            gunParticles = GetComponent<ParticleSystem>();
+            gunLine = GetComponent<LineRenderer>();
+            gunAudio = GetComponent<AudioSource>();
+            gunLight = GetComponent<Light>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            timer += Time.deltaTime;
+
+            if (ShootAction.GetStateDown(HandType))
+            {
+                Shoot();
+            }
+
+            if (timer >= timeBetweenBullets * effectDisplayTime)
+            {
+                DisableEffects();
+            }
+        }
+
+        public void DisableEffects()
+        {
+            gunLine.enabled = false;
+            gunLight.enabled = false;
+        }
+
+        void Shoot()
+        {
+            timer = 0f;
+            gunAudio.Play();
+            gunLight.enabled = true;
+
+            gunParticles.Stop();
+            gunParticles.Play();
+
+            gunLight.enabled = true;
+            gunLine.SetPosition(0, transform.position);
+
+            shootRay.origin = transform.position;
+            shootRay.direction = transform.forward;
+
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+            {
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(damagePerShot, shootHit.point);
+                }
+                gunLine.SetPosition(1, shootHit.point);
+            }
+            else
+            {
+                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ShootingTutorialTest
+SteamVR Plugin ver2 Test(2019)  
+Reference : https://qiita.com/kousaku-maron/items/94be55733a8512260934
